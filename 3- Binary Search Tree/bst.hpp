aaf9b5f64e21402a54481e196257873faa96f2f0
Joao Pedro Alves de Moraes <jpam@WINDOWS.CIN.UFPE.BR>
2025-07-30 16:04:11 -0300

    implementação completa da bst

--- 3- Binary Search Tree/bst.hpp
+++ 3- Binary Search Tree/bst.hpp
@@ -23,9 +23,15 @@ public:
     Node* getLeft() { return left; }
     Node* getRight() { return right; }
 
+    void setKey(int k) { key = k; }
+    void setValue(int v) { value = v; }
+    void setLeft(Node* l) { left = l; }
+    void setRight(Node* r) { right = r; }
+
 };
 
 class Bst {
+private:
     Node* root;
     int nodeCnt;
     void recursiveDeletion(Node* curr) {
@@ -57,8 +63,134 @@ class Bst {
        
     }
 
-    
+    Node* insertHelp(Node* root, int k, int v) {
+        if (root == nullptr) {
+            Node* newNode = new Node(k, v);
+            newNode->setKey(k);
+            newNode->setValue(v);
+            return newNode;
+            
+        }
+        
+        if (root->getKey() > k) {
+            root->setLeft(insertHelp(root->getLeft(), k, v));
+            
+        } else {
+            root->setRight(insertHelp(root->getRight(), k, v));
+            
+        }
+        return root;
+        
+    }
 
+    Node* removeHelp(Node* root, int k) {
+        if (root == nullptr) {
+            return nullptr;
+            
+        }
+        if (root->getKey() > k) {
+            root->setLeft(removeHelp(root->getLeft(), k));
+            
+        } else if (root->getKey() < k) {
+            root->setRight(removeHelp(root->getRight(), k));
+
+        } else {
+            if (root->getLeft() == nullptr) {
+                return root->getRight();
+                
+            } else if (root->getRight() == nullptr) {
+                return root->getLeft();
+                
+            } else {
+                Node* temp = getmin(root->getRight());
+                root->setValue(temp->getValue());
+                root->setKey(temp->getKey());
+                root->setRight(deletemin(root->getRight()));
+
+            }
+            
+        }
+        return root;
+
+    }
+
+public:
+    Bst() : root(nullptr), nodeCnt(0) {}
+
+    ~Bst() { recursiveDeletion(root); }
+
+    int find(int k) {
+        return findHelp(root, k);
+        
+    }
+
+    void insert(int k, int v) {
+        root = insertHelp(root, k, v);
+        nodeCnt++;
+
+    }
+
+    int erase(int k) {
+        int temp = findHelp(root, k);
+        if (temp != -1) {
+            root = removeHelp(root, k);
+            nodeCnt--;
+
+        }
+        return temp;
+
+    }
+
+    Node* getmin(Node* root) {
+        if (root->getLeft() == NULL) {
+            return root;
+            
+        }
+        return getmin(root->getLeft());
+        
+    }
+    
+    Node* deletemin(Node* root) {
+        if (root->getLeft() == NULL) {
+            return root->getRight();
+            
+        }
+        root->setLeft(deletemin(root->getLeft()));
+        return root;
+        
+    }
+
+    // Funções de travessia
+    void preOrder(Node* root) {
+        if (root != nullptr) {
+            std::cout << root->getKey() << " ";
+            preOrder(root->getLeft());
+            preOrder(root->getRight());
+            
+        }
+        
+    }
+    
+    void inOrder(Node* root) {
+        if (root != nullptr) {
+            inOrder(root->getLeft());
+            std::cout << root->getKey() << " ";
+            inOrder(root->getRight());
+            
+        }
+        
+    }
+    
+    void posOrder(Node* root) {
+        if (root != nullptr) {
+            posOrder(root->getLeft());
+            posOrder(root->getRight());
+            std::cout << root->getKey() << " ";
+
+        }
+        
+    }
+    
 };
 
 #endif 
